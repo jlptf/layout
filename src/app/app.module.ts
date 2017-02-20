5b9315be00433f67fe55a567d91fc1a0b58fbdb0
James Lu <jlptf0420@gmail.com>
2017-02-20 14:05:58 +0800

    1. apply adminlte/bootstrap; 2. create widgets for layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './layouts/main.component';
+import { HeaderComponent } from './widgets/header/header.component';
+import { FooterComponent } from './widgets/footer/footer.component';
+import { MenuComponent } from './widgets/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
